b65deb95cb3ca4950eb3cdba050849a8d952ff2e
Hoang Nguyen <frznflm@live.com>
2015-11-10 12:41:06 -0600

    Server + Worker(connection handler)
    Use Bluecat to test

---
+++ test puzzle/src/Server.java
@@ -0,0 +1,102 @@
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Server implements Runnable
+{
+
+	protected int          serverPort   = 8080;
+	protected ServerSocket serverSocket = null;
+	protected boolean      isStopped    = false;
+	protected Thread       runningThread= null;
+	private int Threadcount = 0;
+
+	public Server(int port)
+	{
+		this.serverPort = port;
+	}
+
+	public void run()
+	{
+		synchronized(this)
+		{
+			this.runningThread = Thread.currentThread();
+		}
+		openServerSocket();
+		while(! isStopped())
+		{
+			Socket clientSocket = null;
+			try 
+			{
+				clientSocket = this.serverSocket.accept();
+			} catch (IOException e) 
+			{
+				if(isStopped()) 
+				{
+					System.out.println("Server Stopped.") ;
+					return;
+				}
+				throw new RuntimeException("Error accepting client connection", e);
+			}
+			new Thread(new Worker(clientSocket, "Multithreaded Server", ++Threadcount)).start();
+		}
+		System.out.println("Server Stopped.") ;
+	}
+
+
+	private synchronized boolean isStopped() 
+	{
+		return this.isStopped;
+	}
+
+	public synchronized void stop()
+	{
+		this.isStopped = true;
+		try 
+		{
+			this.serverSocket.close();
+		} 
+		catch (IOException e) 
+		{
+			throw new RuntimeException("Error closing server", e);
+		}
+	}
+
+	private void openServerSocket() 
+	{
+		try 
+		{
+			this.serverSocket = new ServerSocket(this.serverPort);
+		} 
+		catch (IOException e) 
+		{
+			throw new RuntimeException("Cannot open port 8080", e);
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Server server = new Server(9000);
+		new Thread(server).start();
+		BufferedReader input= new BufferedReader( new InputStreamReader(System.in) );
+		String temp = input.readLine();
+		//		try 
+		//		{
+		//    	    Thread.sleep(20 * 1000);
+		//    	} 
+		//		catch (InterruptedException e) 
+		//		{
+		//    	    e.printStackTrace();
+		//    	}
+		while (!temp.equals("stop"))
+		{
+			temp = input.readLine();
+		}
+		System.out.println("Stopping Server");
+		server.stop();
+
+	}
+}
---
+++ test puzzle/src/Worker.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+
+ */
+public class Worker implements Runnable{
+
+    protected Socket clientSocket = null;
+    protected String serverText   = null;
+    private int count = 0;
+
+    public Worker(Socket clientSocket, String serverText, int count) {
+        this.clientSocket = clientSocket;
+        this.serverText   = serverText;
+        this.count = count;
+    }
+
+    public void run() {
+        try {
+        	BufferedReader input= new BufferedReader( new InputStreamReader( clientSocket.getInputStream() ) );
+            PrintWriter output= new PrintWriter( clientSocket.getOutputStream(), true );
+            long time = System.currentTimeMillis();
+            String temp = input.readLine();
+            output.println("Thread" + this.count + " responded");
+            output.close();
+            input.close();
+            System.out.println("Request processed: " + time);
+        } catch (IOException e) {
+            //report exception somewhere.
+            e.printStackTrace();
+        }
+    }
+}
