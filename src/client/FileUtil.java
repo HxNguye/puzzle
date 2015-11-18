ae9c68b57d5324417821b2d076f61b1e225afb03
arendon87 <ajrendon31660@gmail.com>
2015-11-18 09:08:51 -0600

    Created FileUtil class to handle puzzles

---
+++ src/client/FileUtil.java
@@ -0,0 +1,19 @@
+package client;
+
+/**
+ * FileUtil class used to receive puzzles for the client (player) side from the server
+ */
+public class FileUtil 
+{
+	
+	/**
+	 * Constructor
+	 */
+	public FileUtil(Byte byteFile, boolean response, String message)
+	{
+		//TODO: Implement this further
+	}
+	
+	
+
+}
