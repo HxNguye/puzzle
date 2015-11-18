a5cc06f125791d71619ae00732610e605cb57632
arendon87 <ajrendon31660@gmail.com>
2015-11-18 09:08:01 -0600

    Minor changes

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,61 +1,49 @@
 package client;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
 
-@Deprecated 
-/**DO NOT USE YET
- * */
+
 public class Client 
-{
+{	
 	int port_number;
 	InetAddress loopback;
 	InetAddress localhost;
 	Socket client_socket;
 	PrintWriter output;
 	BufferedReader input;
-
-	/**
-	 * The following constructor enables connection between client and a designated port number
-	 */
+	
 	public Client(int port) throws IOException
 	{
 		port_number = port;
 		loopback = InetAddress.getLoopbackAddress();
-
+		
 		// try to connect to the server
 		client_socket = new Socket(loopback, port_number);		
-
+		
 		// grab the output and input streams
 		output = new PrintWriter(client_socket.getOutputStream(), true);
 		input = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
 	}
-
-	/**
-	 * The following is an overload method that connects client's localhost to a designated port number
-	 */
-	public Client(String address, int port) throws IOException
-	{
-		port_number = port;
-		localhost = InetAddress.getLocalHost();
-
-		// try to connect to the server
-		client_socket = new Socket(localhost, port_number);		
-
-		// grab the output and input streams
-		output = new PrintWriter(client_socket.getOutputStream(), true);
-		input = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-	}
-
+	
 	public void communicate(String message) throws IOException
 	{
 		output.println(message);
 		message = input.readLine();
 		System.out.println("Client has received the following message: \n" + message);
 	}
+	
+	
+	
+	
+
+	
 }
 
---
+++ src/client/ClientMain.java
@@ -0,0 +1,15 @@
+package client;
+
+import java.io.IOException;
+
+
+public class ClientMain 
+{
+    public static void main(String [] args) throws IOException
+    {
+    	Client c = new Client(8000);
+    	
+    	
+    }
+
+}
