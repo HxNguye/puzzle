543de19c0acc8ca4709a9462ceb2c9bd3dd6d4a9
arendon87 <ajrendon31660@gmail.com>
2015-11-20 10:07:01 -0600

    Slight modification...needs further implementation

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,15 +1,35 @@
 package client;
-
-import java.io.IOException;
+import javax.swing.JOptionPane;
 
 
 public class ClientMain 
 {
-    public static void main(String [] args) throws IOException
-    {
-    	Client c = new Client(8000);
-    	
-    	
-    }
+
+
+	public static void main(String[] args) throws Exception
+	{
+		Client c = new Client();
+		
+		if ( args.length != 1 )
+		{
+			System.out.println( "Usage: Client -s");
+			System.out.println( "To answer with a file or text message to the server");
+			return;
+		}
+
+		try
+		{
+			c.connect(9000);
+		}
+		catch( Exception e )
+		{
+			JOptionPane.showMessageDialog(null, "Server is not started. Can not connect!");
+		}
+		
+		
+		
+
+
+	}
 
 }
