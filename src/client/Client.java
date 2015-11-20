ab92ee101d166ad09739b5433470f82058babccb
arendon87 <ajrendon31660@gmail.com>
2015-11-20 10:06:36 -0600

    Reworked the Client method...need to implement other classes

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,46 +1,118 @@
 package client;
+import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
-
-
 
 public class Client 
 {	
 	int port_number;
-	InetAddress loopback;
-	InetAddress localhost;
-	Socket client_socket;
-	PrintWriter output;
-	BufferedReader input;
-	
-	public Client(int port) throws IOException
+	String localhost = "127.0.0.1";
+	Socket clientSocket;
+	InputStream in = null;
+	OutputStream out = null;
+	PrintWriter outToServer;
+	BufferedReader inFromServer;
+
+	TimerUtil timer = new TimerUtil();
+	ScoringUtil score = new ScoringUtil();
+	RankingEnum rank;
+
+
+	public Client(){}
+
+
+	public void connect(int port) throws IOException
 	{
-		port_number = port;
-		loopback = InetAddress.getLoopbackAddress();
-		
-		// try to connect to the server
-		client_socket = new Socket(loopback, port_number);		
-		
-		// grab the output and input streams
-		output = new PrintWriter(client_socket.getOutputStream(), true);
-		input = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+		try
+		{
+			port_number = port;		
+			// try to connect to the server
+			clientSocket = new Socket(localhost, port_number);		
+
+			// grab the output and input streams
+			outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
+			inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			// first initial score
+			score.addScore(100);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Can't accept client connection. ");
+		}
+
+		try
+		{
+			in = clientSocket.getInputStream();
+		}
+		catch (Exception e)
+		{
+			System.out.println("Can't get socket input stream. ");
+		}
+
+		byte[] bytes = new byte[1024];
+
+		int count;
+		while ((count = in.read(bytes)) > 0)
+		{
+			out.write(bytes, 0, count);
+		}
+
+
+		out.close();
+		in.close();
 	}
-	
+
 	public void communicate(String message) throws IOException
 	{
-		output.println(message);
-		message = input.readLine();
-		System.out.println("Client has received the following message: \n" + message);
+		try
+		{
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			//			String fromServer;
+			String fromClient = message;
+			fromClient = stdIn.readLine();
+			while(fromClient != null)
+			{
+				outToServer.println(fromClient);
+			}	
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
-	
-	
-	
 
-	
+	public void FileHelper(byte[] byteFile, boolean response, String message) throws FileNotFoundException
+	{
+		// if correct answer
+		if (response = true)
+		{
+			File file = new File("");
+			// convert array of bytes into a file
+			FileOutputStream fileOutputStream = new FileOutputStream(file);
+			try
+			{
+				fileOutputStream.write(byteFile);
+				fileOutputStream.close();
+
+				// open the file
+				Desktop.getDesktop().open(file);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+
+			score.addScore(1000);
+			System.out.println(message);
+		}
+	}
 }
 
